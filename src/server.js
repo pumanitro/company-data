c8c255180b4880f0053d2ca68cd783b67f71a074
Patryk Janik <patryk.janik@castingnetworks.com>
2019-09-28 19:11:31 +0200

    Added cors support to app.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import CONSTS from './consts';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 80;
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('We keep here all our up to date company data'));
 
